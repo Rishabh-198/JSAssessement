a0b6dc2c080dff97bd9c01753c28fb6f9f3c72f8
Rishabh-198 <93992066+Rishabh-198@users.noreply.github.com>
2023-06-21 18:46:17 +0530

    Create mintNFT.js

---
+++ mintNFT.js
@@ -0,0 +1,31 @@
+const NFTs=[]
+function mintNFT (name, year, country, category) {
+    const NFT={
+        name:name, year: year, country: country, category: category
+    }
+    NFTs.push(NFT);
+}
+function listNFTs () {
+    console.log("NFTs are:");
+for(let i=0;i<NFTs.length;i++)
+{
+    console.log(i+1+".");
+    console.log("Name: "+NFTs[i].name);
+    console.log("Year: "+NFTs[i].year);
+    console.log("Country: "+NFTs[i].country);
+    console.log("Category: "+NFTs[i].category);
+    console.log("\n");
+
+}
+}
+function getTotalSupply() {
+console.log("The total number of NFTs are:"+NFTs.length);
+}
+mintNFT("Rishabh",2003,"India", "Picture");
+mintNFT("BMW",2000,"USA", "Car");
+mintNFT("Mitali",2020,"Pakistan"," Ghost");
+mintNFT("Noodles",1987,"China", "Food");
+mintNFT("Ripped Shirt",2013,"Sweden", "Clothes");
+listNFTs();
+getTotalSupply();
+
